a202291e745b9de851cd795c967e46856bc80957
付羽凡 <5288595+fu_yufan@user.noreply.gitee.com>
2024-05-25 23:56:32 +0800

    feat:信息流ItemUI完成90%，以及增加多图逻辑处理

--- src/components/informationFlow/flowList/index.tsx
+++ src/components/informationFlow/flowList/index.tsx
@@ -1,37 +1,66 @@
+import { EImagePosition } from "../../../type/enum";
+import { ISinglePictureItemType } from "../../../type/informationFlow";
 import SinglePictureItem from "../singlePictureItem";
 import styles from "./index.module.scss";
 
 const InformationFlowList = () => {
-
   //TODO:待修改
-  const data = [
+  const data: ISinglePictureItemType[] = [
     {
       content:
         "一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极 一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极 一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极",
       commentInfo: "174评",
       author: "惜蕊说历史",
-      image:
+      imagePosition: EImagePosition.right,
+      image: [
         "https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=60",
+      ],
     },
     {
       content:
         "一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极 一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极 一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极",
       commentInfo: "50评",
       author: "小易寒",
-      image:
+      imagePosition: EImagePosition.left,
+      image: [
         "https://images.unsplash.com/photo-1620476214170-1d8080f65cdb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80",
+      ],
     },
     {
       content:
         "一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极 一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极 一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极",
       commentInfo: "50评",
       author: "小易寒",
-  },
+      imagePosition: EImagePosition.bottom,
+      image: [
+        "https://images.unsplash.com/photo-1620476214170-1d8080f65cdb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80",
+        "https://images.unsplash.com/photo-1620476214170-1d8080f65cdb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80",
+      ],
+    },
+    {
+      content:
+        "一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极 一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极 一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极",
+      commentInfo: "50评",
+      author: "小易寒",
+      imagePosition: EImagePosition.bottom,
+      image: [
+        "https://images.unsplash.com/photo-1620476214170-1d8080f65cdb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80",
+        "https://images.unsplash.com/photo-1620476214170-1d8080f65cdb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80",
+        "https://images.unsplash.com/photo-1620476214170-1d8080f65cdb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80",
+      ],
+    },
+    {
+      content:
+        "一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极 一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极 一旦我国武统台湾,美将 打击中方核武库和火箭军, 美司令嚣张至极",
+      commentInfo: "50评",
+      author: "小易寒",
+      image:undefined
+    },
   ];
 
   return (
     <div className={styles.container}>
-      {data.map((item,key) => (
+      {data.map((item, key) => (
         <SinglePictureItem {...item} key={key} />
       ))}
     </div>
--- src/components/informationFlow/singlePictureItem/index.tsx
+++ src/components/informationFlow/singlePictureItem/index.tsx
@@ -1,28 +1,117 @@
 import styles from "./index.module.scss";
 import { CloseOutline } from "antd-mobile-icons";
 import { Ellipsis, Image } from "antd-mobile";
-import React from "react";
-
-interface ISinglePictureItemType {
-  content?: string;
-  commentInfo?: string;
-  author?: string;
-  image?: string;
-}
+import React, { useMemo } from "react";
+import { ISinglePictureItemType } from "../../../type/informationFlow";
+import { EImagePosition } from "../../../type/enum";
 
 const SinglePictureItem: React.FC<ISinglePictureItemType> = ({
   content,
   commentInfo,
   author,
-  image,
+  image = [],
+  imagePosition,
 }) => {
+  const relativeInformation = useMemo(() => {
+    return (
+      <div className={styles.otherInformation}>
+        <div>
+          <span>{author}</span>
+          <span> {commentInfo}</span>
+        </div>
+        <div className={styles.iconContainer}>
+          <CloseOutline className={styles.icon} />
+        </div>
+      </div>
+    );
+  }, [author, commentInfo]);
+
+  const renderPicture = useMemo(() => {
+    if (!imagePosition) {
+      return;
+    }
+
+    if (image.length === 0) {
+      return;
+    }
+
+    const renderImage = <Image lazy src={image[0] || ""} />;
+    const pictureMaps = new Map<EImagePosition, JSX.Element>([
+      [
+        EImagePosition.left,
+        <div
+          className={
+            imagePosition === EImagePosition.left
+              ? [styles.singlePicture, styles.setRightMargin].join(" ")
+              : styles.singlePicture
+          }
+        >
+          {renderImage}
+        </div>,
+      ],
+      [
+        EImagePosition.right,
+        <div className={styles.singlePicture}>{renderImage}</div>,
+      ],
+      [
+        EImagePosition.bottom,
+        <div className={styles.multiPicture}>
+          <div className={styles.pictures}>
+            {image.map((imageUrl, key) => (
+              <Image
+                src={imageUrl || ""}
+                key={key}
+                className={styles.imageStyle}
+              />
+            ))}
+            {image.length === 2 && <div className={styles.placeholder} />}
+          </div>
+          {relativeInformation}
+        </div>,
+      ],
+    ]);
+
+    return pictureMaps.get(imagePosition);
+  }, [imagePosition, image, relativeInformation]);
+
+  const renderOtherInformation = useMemo(() => {
+    if (imagePosition === EImagePosition.bottom) {
+      return;
+    }
+
+    return relativeInformation;
+  }, [imagePosition, relativeInformation]);
+
+  const containerStyle = useMemo(() => {
+    if (!imagePosition) {
+      return styles.container;
+    }
+
+    const containerStylesMap = new Map([
+      [
+        EImagePosition.left,
+        [styles.setImageLeftPosition, styles.container].join(" "),
+      ],
+      [
+        EImagePosition.bottom,
+        [styles.setImageBottomPosition, styles.container].join(" "),
+      ],
+    ]);
+
+    if (!containerStylesMap.has(imagePosition)) {
+      return styles.container;
+    }
+
+    return containerStylesMap.get(imagePosition);
+  }, [imagePosition]);
+
   return (
-    <div className={styles.container}>
+    <div className={containerStyle}>
       <div
         className={
-          image
+          image.length === 1
             ? styles.textContent
-            : [styles.textContent,styles.noPicture].join(" ")
+            : [styles.textContent, styles.noPicture].join(" ")
         }
       >
         <div className={styles.main}>
@@ -33,21 +122,9 @@ const SinglePictureItem: React.FC<ISinglePictureItemType> = ({
             expandText="展开"
           />
         </div>
-        <div className={styles.subordination}>
-          <div>
-            <span>{author}</span>
-            <span> {commentInfo}</span>
-          </div>
-          <div className={styles.iconContainer}>
-            <CloseOutline className={styles.icon} />
-          </div>
-        </div>
+        {renderOtherInformation}
       </div>
-      {image && (
-        <div className={styles.singlePicture}>
-          <Image lazy src={image || ""} />
-        </div>
-      )}
+      {renderPicture}
     </div>
   );
 };
